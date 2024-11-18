e3e24e8e04190da71f71eabc448333c0998e3421
so635 <yoooyooosy@gmail.com>
2024-11-18 17:31:03 +0300

    Add files via upload

---
+++ js/web.js
@@ -0,0 +1,22 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="f0f0a981-6560-41a4-839b-36ee1d2f97c4";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+
+// <!--Start of Tawk.to Script-->
+
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/6739936d2480f5b4f59f3965/1icsefq0a';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+
+// <!--End of Tawk.to Script-->
+
+let icon = document.getElementById('icon-mobile')
+let ul = document.getElementById('ul-mobile')
+
+icon.onclick = function () {
+    ul.classList.toggle("mobile-nav1")
+}
